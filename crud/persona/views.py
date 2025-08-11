4c4fe89d889483b1993a4c2df866c9d50cadd843
sofisp <sofisapo927@gmail.com>
2025-08-11 20:45:23 -0300

    Búsqueda de personas, título en listado de personas

--- crud/persona/views.py
+++ crud/persona/views.py
@@ -12,6 +12,11 @@ class PersonaListView(ListView):
     template_name = "persona/lista.html"
     context_object_name = "personas"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = "Listado de Personas"
+        return context
+
 class PersonaDetailView(DetailView):
     model = Persona
     template_name = "persona/detalle.html"
@@ -41,4 +46,21 @@ class PersonaDeleteView(LoginRequiredMixin, DeleteView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['action'] = 'eliminar'
+        return context
+
+class PersonaSearchView(ListView):
+    model = Persona
+    template_name = "persona/buscar.html"
+    context_object_name = "personas"
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        if query:
+            return Persona.objects.filter(nombre__icontains=query)
+        return Persona.objects.none()
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Buscar Persona'
+        context['query'] = self.request.GET.get("q", "")
         return context
