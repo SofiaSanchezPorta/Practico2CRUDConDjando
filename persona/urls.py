572084b718d0494d9c25f8eb5e4b63fa971f787c
sofisp <sofisapo927@gmail.com>
2025-08-19 20:50:59 -0300

    Template base y persona/lista.html (para visualización de lista de personas) con ListView. Agregado de decorador para crear y buscar con Django.admin. Creación de vista PersonaListView y del path correspondiente.

---
+++ persona/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+app_name = "persona"
+
+urlpatterns = [
+    path(
+        'lista/',
+        PersonaListView.as_view(),
+        name='lista'
+    )
+]
+
