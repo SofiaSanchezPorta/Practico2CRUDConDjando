d3613d91d3cee81590942be53e45cc0483972881
sofisp <sofisapo927@gmail.com>
2025-08-19 21:48:44 -0300

    Vistas y templates de CRUD con paths correspondientes agregados a personas.urls.py

--- persona/views.py
+++ persona/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Persona
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class PersonaListView(ListView):
     model = Persona
     template_name = "persona/lista.html"
     context_object_name = "personas"
 
+class PersonaDetailView(DetailView):
+    model = Persona
+    template_name = "persona/detalle.html"
+    context_object_name = "persona"
+
+class PersonaCreateView(LoginRequiredMixin, CreateView):
+    model = Persona
+    template_name = "persona/crear.html"
+    fields = ['nombre', 'apellido', 'edad', 'oficina']
+    success_url = reverse_lazy('persona:lista')
+
+class PersonaUpdateView(LoginRequiredMixin, UpdateView):
+    model = Persona
+    template_name = "persona/crear.html"
+    fields = ['nombre', 'apellido', 'edad', 'oficina']
+    success_url = reverse_lazy('persona:lista')
+
+class PersonaDeleteView(LoginRequiredMixin, DeleteView):
+    model = Persona
+    template_name = "persona/eliminar.html"
+    context_object_name = "persona"    
+    success_url = reverse_lazy('persona:lista')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['action'] = 'eliminar'
+        return context
